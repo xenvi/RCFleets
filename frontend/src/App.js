f67c2321834475a0fa9123d6feebcb9c8f9791a0
xenvi <iluvpups20@gmail.com>
2021-10-25 23:25:13 -0600

    setup redux store for auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+// redux
+import { Provider } from 'react-redux';
+import store from './store';
 // hoc
 import Layout from './hoc/Layout';
-
 // containers
 import Home from './containers/Home';
 import Login from './containers/Login';
@@ -13,18 +14,20 @@ import ResetPassword from './containers/ResetPassword';
 import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 
 const App = () => (
-    <Router>
-        <Layout>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
-                <Route exact path="/activate/:uid/:token" component={Activate} />
-                <Route exact path="/reset-password" component={ResetPassword} />
-                <Route exact path="/password/reset/confirm/:uid/:token" component={ResetPasswordConfirm} />
-            </Switch>
-        </Layout>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={Signup} />
+                    <Route exact path="/activate/:uid/:token" component={Activate} />
+                    <Route exact path="/reset-password" component={ResetPassword} />
+                    <Route exact path="/password/reset/confirm/:uid/:token" component={ResetPasswordConfirm} />
+                </Switch>
+            </Layout>
+        </Router>
+    </Provider>
 );
 
 export default App;
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { login } from '../actions/auth';
 
-const Login = () => (
-    <div>
-        Login
-    </div>
-);
+const Login = ({ login }) => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
 
-export default Login;
+    const { email, password } = formData;
+
+    const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        login(email, password);
+    };
+
+    // is user authenticated? redirect to homepage
+
+    return (
+        <div>
+            <h1>Log In</h1>
+            <form onSubmit={(e) => onSubmit(e)}>
+                <div>
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      name="email"
+                      value={email}
+                      onChange={(e) => onChange(e)}
+                      required
+                    />
+                </div>
+                <div>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      value={password}
+                      onChange={(e) => onChange(e)}
+                      minLength="6"
+                      required
+                    />
+                </div>
+                <button type="submit">
+                    Log In
+                </button>
+            </form>
+            <p>
+                Don&apos;t have an account?
+                <Link to="signup">Sign Up</Link>
+            </p>
+            <p>
+                Forgot your Password?
+                <Link to="reset-password">Reset Password</Link>
+            </p>
+        </div>
+    );
+};
+
+// const mapStateToProps = state => ({
+//     // is authenticated
+// });
+
+export default connect(null, { login })(Login);
